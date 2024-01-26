1b17f57658c42aaedcbf2e23ad04de063e8fb18f
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2024-01-26 21:28:19 +0100

    refactor: refactor code

---
+++ src/components/navigation/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { PiWalletFill } from "react-icons/pi";
+import { FaCompass } from "react-icons/fa";
+import { FaBell, FaGear } from "react-icons/fa6";
+
+const Navigation = () => {
+  // const [activeNav, setActiveNav] = useState('#wallet');
+
+  return (
+    <nav>
+      <a href="#wallet">
+        <PiWalletFill />
+      </a>
+      <a href="#">
+        <FaCompass/>
+      </a>
+      <a href="#">
+        <FaBell/>
+      </a>
+      <a href="#">
+        <FaGear/>
+      </a>
+    </nav>
+  );
+};
+
+export default Navigation;
